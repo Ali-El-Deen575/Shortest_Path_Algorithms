3f5f7f795f7bbfd2359dfd9c7412d9f3daf25a98
omar <omar2004khaled@gmail.com>
2025-04-17 14:05:39 -0800

    testcases done

--- ShortestPathAlgorithms'/Test/DijekstraTest.java
+++ ShortestPathAlgorithms'/Test/DijekstraTest.java
@@ -245,4 +245,93 @@ public class DijekstraTest {
         assertEquals(1, costs[999], "Costs array does not match expected values");
         assertEquals(ans, path, "Parents array does not match expected values");
     }
+    @Test
+public void testDisconnectedGraph() throws IOException {
+    String filePath = "ShortestPathAlgorithms'\\resources\\disconnectedGraph.txt"; 
+    Graph graph = new Graph(filePath);
+
+    int[] parents = new int[graph.Size()];
+    int[] costs = new int[graph.Size()];
+
+    Vertex source = graph.getVertices().get(0);
+    graph.Dijekstra(source, parents, costs);
+
+    List<Integer> path = graph.getPath(0, 3, parents);
+    assertEquals(Integer.MAX_VALUE, costs[3], "Node should be unreachable");
+    assertTrue(path.isEmpty(), "Path should be empty for unreachable node");
+}
+@Test
+public void testZeroEdgeWeights() throws IOException {
+    String filePath = "ShortestPathAlgorithms'\\resources\\zeroEdgeWeights.txt"; 
+    Graph graph = new Graph(filePath);
+
+    int[] parents = new int[graph.Size()];
+    int[] costs = new int[graph.Size()];
+
+    Vertex source = graph.getVertices().get(0);
+    graph.Dijekstra(source, parents, costs);
+
+    List<Integer> expectedPath = Arrays.asList(0, 1, 2);
+    List<Integer> actualPath = graph.getPath(0, 2, parents);
+    assertEquals(0, costs[2], "Total cost should be 0 due to zero-weight edges");
+    assertEquals(expectedPath, actualPath);
+}
+@Test
+public void testMultipleSameCostPathsDeterministic() throws IOException {
+    String filePath = "ShortestPathAlgorithms'\\resources\\sameCostPaths.txt"; 
+    Graph graph = new Graph(filePath);
+
+    int[] parents = new int[graph.Size()];
+    int[] costs = new int[graph.Size()];
+
+    Vertex source = graph.getVertices().get(0);
+    graph.Dijekstra(source, parents, costs);
+
+    List<Integer> actualPath = graph.getPath(0, 3, parents);
+    assertEquals(5, costs[3], "Cost to destination should be 5");
+
+    // One of these should be correct depending on implementation
+    List<Integer> expected1 = Arrays.asList(0, 1, 3);
+    List<Integer> expected2 = Arrays.asList(0, 2, 3);
+    assertTrue(actualPath.equals(expected1) || actualPath.equals(expected2));
+}
+
+@Test
+public void testAllNodesReachable() throws IOException {
+    String filePath = "ShortestPathAlgorithms'\\resources\\allReachable.txt"; 
+    Graph graph = new Graph(filePath);
+
+    int[] parents = new int[graph.Size()];
+    int[] costs = new int[graph.Size()];
+
+    Vertex source = graph.getVertices().get(0);
+    graph.Dijekstra(source, parents, costs);
+
+    for (int i = 1; i < graph.Size(); i++) {
+        assertTrue(costs[i] < Integer.MAX_VALUE, "Node " + i + " should be reachable");
+        List<Integer> path = graph.getPath(0, i, parents);
+        assertFalse(path.isEmpty(), "Path to node " + i + " should not be empty");
+    }
+}
+
+@Test
+public void additionalTest() throws IOException {
+    String filePath = "ShortestPathAlgorithms'\\resources\\test6.txt"; 
+    Graph graph = new Graph(filePath);
+
+    int[] parents = new int[graph.Size()];
+    int[] costs = new int[graph.Size()];
+
+    Vertex source = graph.getVertices().get(0);
+    graph.Dijekstra(source, parents, costs);
+
+    List<Integer> expectedPath = Arrays.asList(0, 2, 6);
+    List<Integer> actualPath = graph.getPath(0, 6, parents);
+
+    assertEquals(5, costs[6], "Costs array does not match expected values");
+    assertEquals(expectedPath, actualPath, "Parents array does not match expected values");
+}
+
+
+
 }
